061ad7fd28854a41a3936f2d4f58c47ae9922cfc
Hotery <tinkey97@gmail.com>
2024-01-07 14:40:14 +0100

    goit-js-hw-07

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,7 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
-}
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
 
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener('input', () => {
+  const name = nameInput.value.trim() || 'Anonymous';
+  nameOutput.textContent = `Hello, ${name}!`;
+});
